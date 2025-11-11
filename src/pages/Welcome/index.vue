a0a352f5d842952152ea36eb3b2a0c3d51efa2fc
Miguel2k20 <joaomiguel2k20@gmail.com>
2025-11-11 01:03:47 -0300

    Tela de dashboard

--- src/pages/Welcome/index.vue
+++ src/pages/Welcome/index.vue
@@ -1,6 +1,42 @@
 <template>
     <section id="section">
-        <h1>Boa noite, Clara!</h1>
-        <p>Estamos felizes em tê-la conosco!</p>
+        <h1>{{ welcomeMessage }}</h1>
+        <p>Estamos felizes em ter você conosco!</p>
     </section>
 </template>
+<script>
+export default {
+    name: "WelcomePage",
+    data(){
+        return {
+            welcomeMessage: null
+        }
+    },
+    created(){
+        this.fetchUserName()
+    },
+    methods: {
+        fetchUserName() {
+            const user = JSON.parse(localStorage.getItem('user'));
+            if (user && user.name) {
+                this.userName = user.name;
+
+                const hora = new Date().getHours();
+                let saudacao = '';
+
+                if (hora >= 5 && hora < 12) {
+                saudacao = 'Bom dia';
+                } else if (hora >= 12 && hora < 18) {
+                saudacao = 'Boa tarde';
+                } else if (hora >= 18 && hora < 24) {
+                saudacao = 'Boa noite';
+                } else {
+                saudacao = 'Boa madrugada';
+                }
+
+                this.welcomeMessage = `${saudacao}, ${this.userName}!`;
+            }
+        }
+    }
+};
+</script>
